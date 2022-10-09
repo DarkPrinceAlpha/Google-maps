1f65908808e85b7e4ccb794f57c551d64e53ae38
Ali Najfi <ali.najfi009@gmail.com>
2022-10-09 22:11:23 +0500

    basic tasks with google map

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -16,6 +17,7 @@ class _HomePageState extends State<HomePage> {
   late LatLng cuurentPostion;
   bool _buildErrorOrMap = true;
   final Completer<GoogleMapController> _controller = Completer();
+  String address = '';
   final List<Marker> _marker = [];
   final List<Marker> _list = [
     const Marker(markerId: MarkerId('1'), position: LatLng(29.3544, 71.6911)),
@@ -40,17 +42,25 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: _buildErrorOrMap
-            ? const Center(child: CircularProgressIndicator())
-            : GoogleMap(
-                onMapCreated: (controller) {
-                  _controller.complete(controller);
-                },
-                markers: Set<Marker>.of(_marker),
-                myLocationEnabled: true,
-                compassEnabled: true,
-                initialCameraPosition:
-                    CameraPosition(target: cuurentPostion, zoom: 13)),
+        body: Column(
+          children: [
+            Expanded(flex: 2, child: Text(address)),
+            Expanded(
+                flex: 1,
+                child: TextButton(
+                    onPressed: () async {
+                      List<Location> list = await locationFromAddress(
+                          '9MH9+H5M, Bahawalpur Cantt, Bahawalpur, Punjab');
+
+                      setState(() {
+                        address =
+                            "${list.first.latitude}${list.first.latitude}";
+                      });
+                    },
+                    child: const Text('Convert'))),
+            Expanded(flex: 7, child: googleMap()),
+          ],
+        ),
         floatingActionButton: FloatingActionButton(onPressed: () async {
           GoogleMapController controller = await _controller.future;
           controller.animateCamera(
@@ -79,5 +89,19 @@ class _HomePageState extends State<HomePage> {
     } else {
       log('No permission');
     }
+  }
+
+  Widget googleMap() {
+    return _buildErrorOrMap
+        ? const Center(child: CircularProgressIndicator())
+        : GoogleMap(
+            onMapCreated: (controller) {
+              _controller.complete(controller);
+            },
+            markers: Set<Marker>.of(_marker),
+            myLocationEnabled: true,
+            compassEnabled: true,
+            initialCameraPosition:
+                CameraPosition(target: cuurentPostion, zoom: 13));
   }
 }
